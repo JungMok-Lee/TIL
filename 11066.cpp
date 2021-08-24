c4c75af097bd3bf498a4ff83c253e9fec1c85f64
JungMok-Lee <gracelee1020@naver.com>
2021-08-24 22:19:56 +0900

    20210824-19-2

--- 11066.cpp
+++ 11066.cpp
@@ -17,9 +17,13 @@ int main(){
         for(int i=1;i<=k;i++) {
             scanf("%d", &c[i]);
             sum[i]=sum[i-1]+c[i];
-            // dp[i][i] 갱신
         }
 
+        /*
+            dp[i][j] : i번째 에서 j번째까지 파일을 정리할 떄의 최솟값.
+            dp[i][i+1]을 모든 i에 대해서 계산해주고, 그다음은 dp[i][i+2], dp[i][i+3] ... 쭉쭉 이런 식으로 계산해 나가면 된다.
+            dp[i][i] 
+        */
         for(int dif=1;dif<k;dif++){
             for(int tx=1;tx+dif<=k;tx++){
                 int ty=tx+dif;
