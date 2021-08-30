572e72605e256520e2bdbf9f9454b67cbb628fa9
JungMok-Lee <gracelee1020@naver.com>
2021-08-30 14:12:55 +0900

    20210830-re-almight

--- 11053.cpp
+++ 11053.cpp
@@ -1,31 +1,23 @@
 #include <cstdio>
-#include <algorithm>
+int a[1010];
+int dp[1010]; // dp[i] : i번쨰를 무조건 포함하는 LIS!!!
 
-/*  
-    이 문제 풀이가 굉장히 많지만 일단 dp로.
-    dp[i] : i번째 원소를 포함하는 가장 긴 증가하는 부분수열의 길이
-    이렇게 하면 dp[i]가 
-    a[i] 이전의 원소들 중에서 a[i]보다 작은 i의 dp[i]+1 한 것이다. (이전 원소들 중 현재 원소보다 작은 것들 중에 가장 긴 부분수열 길이에, 현재 원소를 붙여준다.)
-    이렇게 되면 dp 중 max를 찾으면 문제가 바로 풀린다.
-*/
 int main(){
-    int n, ans;
-    int a[1001];
-    int dp[1001];
-
+    int n;
     scanf("%d", &n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-        dp[i]=1;
+
+    for(int i=0;i<n;i++) scanf("%d", &a[i]);
+
+    dp[0]=1;
+    for(int i=1;i<n;i++){
+        int max=0;
+        for(int j=0;j<i;j++) if(a[j]<a[i] && max<dp[j]) max=dp[j];
+        dp[i]=max+1;
     }
 
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(a[i]>a[j] && dp[i]<dp[j]+1) dp[i]=dp[j]+1;
-            // if(a[i]>a[j]) dp[i]=max(dp[i], dp[j+1]) 과 동일
-        }
-    }
-    int max=0;
-    for(int i=0;i<n;i++) if(dp[i]>max) max=dp[i];
-    printf("%d\n",max);
+    int ans=0;
+    for(int i=0;i<n;i++)
+        if(ans<dp[i]) ans=dp[i];
+    
+    printf("%d\n", ans);
 }
