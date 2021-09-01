eb1a5293ee0c827fc6b48d4493be014f06b0e993
JungMok-Lee <gracelee1020@naver.com>
2021-09-01 23:30:01 +0900

    20210901-22

--- 1912.cpp
+++ 1912.cpp
@@ -1,21 +1,24 @@
 #include <cstdio>
-int dp[100001];
 int main(){
     int n;
+    int a[1000010];
+    int dp[1000010];
+
     scanf("%d", &n);
+    for(int i=1;i<=n;i++){
+        scanf("%d", &a[i]);
+    }
 
-    int a[100001];
-    for(int i=0;i<n;i++) scanf("%d", &a[i]);
+    dp[1]=a[1];
+    int max=dp[1];
+    for(int i=2;i<=n;i++){
+        // dp[i-1]이 양수면 뭘 더해도 훨씬 낫다. 
+        // 1. 음수에다가 양수 더하기 -> 그냥 자기자신 선택하는게 더 나음.
+        // 2. 음수에다가 음수 더하기 -> 차라리 자기자신만 선택하는게 더 낫다.
+        if(dp[i-1]>0) dp[i]=dp[i-1]+a[i]; 
+        else dp[i]=a[i];
 
-    dp[0]=a[0];
-    int max=a[0];
-    for(int i=1;i<n;i++){
-        if(dp[i-1]>0) dp[i]=dp[i-1]+a[i]; // dp[i-1]이 양수면 그 뒤에 뭘 더해도 거기서부터 시작하는것보다 낫다.
-        else dp[i]=a[i]; // 근데 음수면 거기다 뭘 더하는것보다 앞에거 버리고 거기서부터 시작하는게 낫다.
-
-        // 더 나은 것들을 골라냈으니까 거기 중에서 뭐가 젤 나은지 찾아내야한다.
         if(max<dp[i]) max=dp[i];
     }
-    printf("%d", max);
-
+    printf("%d\n", max);
 }
