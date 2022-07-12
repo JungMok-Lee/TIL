439fe529d426e463c4bfd9083df378d697aafc3c
JungMok-Lee <gracelee1020@naver.com>
2022-07-12 17:16:09 +0900

    20220712-2629-again

--- 2629.cpp
+++ 2629.cpp
@@ -1,39 +1,34 @@
 #include <cstdio>
+#include <cmath>
+int c, g; // c: chu, g: goosle
+int cw[31];
+int gw[8];
 
-int cn;
-int chu[31];
-int bn;
-int b[8];
-
-bool dp[31][50000]={false, }; // dp[i][j] : i번째까지 담아서 j무게를 만들 수 있는가?
+bool dp[31][45000]={false, };// i번째까지 고려했을 때 j무게를 만들 수 있는가?
 
 void f(int i, int w){
-    if(i>cn) return;
+    if(i>c) return;
     if(w>40000) return;
-    if(dp[i][w]) return;
+    if(dp[i][w]) return; // 이거 안 넣어주면 시간 초과 난다!!!!!!
 
     dp[i][w]=true;
-
-    f(i+1, w+chu[i]);
+    f(i+1, w+cw[i]);
     f(i+1, w);
-    if (w-chu[i]>=0) f(i+1, w-chu[i]);
-    else f(i+1, chu[i]-w);
+    f(i+1, abs(w-cw[i]));
 }
 
 int main(){
-    scanf("%d", &cn);
-    for(int i=0;i<cn;i++) scanf("%d", &chu[i]);
-    int bn;
-    scanf("%d", &bn);
-    for(int i=0;i<bn;i++) scanf("%d", &b[i]);
+    scanf("%d", &c);
+    for(int i=0;i<c;i++) scanf("%d", &cw[i]);
+    scanf("%d", &g);
+    for(int i=0;i<g;i++) scanf("%d", &gw[i]);
 
-    f(0,0);
+    f(0, 0);
 
-    for(int i=0;i<bn;i++){
-        if(b[i]>15000) printf("%s ", "N");
-        else if(dp[cn][b[i]]) printf("%s ", "Y");
+    for(int i=0;i<g;i++){
+        if(gw[i]>30*500) printf("%s ", "N");
+        else if(dp[c][gw[i]]) printf("%s ", "Y");
         else printf("%s ", "N");
     }
     printf("\n");
-    
 }
